3d8d01aaf459b43de5ee127f8b18f9b4581e117e
Murali <tarwin1@zohomail.com>
2020-09-22 15:46:08 +0800

    Prototyping stage

--- BOO.sh
+++ BOO.sh
@@ -1,36 +1,59 @@
 #!/bin/sh
 
-echo "WARNING: YOU ARE RUNNING TARWIN's {BOO}tstraping script Make sure you are running this as root\nMakes sure you have connected to internet"
+echo "WARNING: YOU ARE RUNNING TARWIN's {BOO}tstraping script Make sure you are running this as root\nMakes sure you have connected to internet\nFor better performance install dash and setup it up"
+
+read -rp "Would you like to proceed? [N/y] " proceed
 
 main () {
-	timedatectl set-ntp true
+	timedatectl set-ntp true # sets date and time correctly
+	update
+	arch_mirror
+	create_user
+	create_passwd
+	microcode_install
+	ssd_fstrim
+	gpu_driver
+	su $user_name
+	install_yay
+	install_packages
+	setup_dotfiles
+	systemctl_enable
 }
 
 update () {
 	echo 'Updating...'
-	pacman -Syyuu
+	pacman -Syyuu -y
 	echo 'Cleaning cache'
-	pacman -Scc
+	pacman -Scc -y
 
 }
 
 create_user () {
-	echo 'Creating User'
-	echo 'User name must be lower case and no space'
+	echo "Creating User\nUser name must be lower case and no space"
 	read -rp 'Enter you user name: ' user_name
 	user_name=$(echo "$user_name" | tr 'A-Z' 'a-z') # Changes user name to lowercase
-	useradd -mg wheel "$user_name"
+	useradd -mG wheel "$user_name"
+	}
+
+create_passwd () {
 	read -rp "Enter password for $user_name :" user_password
 	echo "$user_password" | passwd --stdin "$user_name"
-}
+	sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
-user_nopass () {
-	echo "$user_name ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 }
 
 install_packages () {
-	pacman -S --needed
-	pacman -S reflector
+	sudo pacman ---needed -S \
+		alsa-utils bspwm dash stow dmenu dunst git htop iwd lf \
+		libnotify libva-utils linux-firmware man-db mlocate mpv neofetch \
+		neovim networkmanager newsboat noto-fonts noto-fonts-emoji picom \
+		rtorrent sxhkd ttf-inconsolata ttf-inconsolata ttf-joypixels \
+		ttf-linux-libertine ttf-symbola uclutter wget xclip xorg-server \
+		xorg-xev xorg-xinit xorg-xprop xorg-xrandr xwallpaper \
+		youtube-dl zathura zathura-pdf-poppler pandoc -y
+
+	yay -S networkmanager-iwd libxft-bgra polybar slock-gruvbox-lowcontrast \
+		st-luke-git
 }
 
 install_yay () {
@@ -38,19 +61,23 @@ install_yay () {
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -si
+	cd "$HOME"
+	rm -rf yay
 }
 
 systemctl_enable () {
-	systemctl enable fstrim.timer
-	systemctl enable NetworkManager
+	sudo systemctl enable fstrim.timer
+	sudo systemctl enable NetworkManager
+	sudo systemctl enable iwd
 }
 
 microcode_install () {
+	echo "Installing microcode"
 	cpu_vendor=$(lscpu | grep Vendor | awk -F ': +' '{print $2}')
 	if [ "$cpu_vendor" = "GenuineIntel" ]; then
-		pacman -S intel-ucode
+		pacman -S intel-ucode -y
 	else
-		pacman -S amd-ucode
+		pacman -S amd-ucode -y
 	fi
 # grub-mkconfig -o /boot/grub/grub.cfg
 }
@@ -63,8 +90,10 @@ ssd_fstrim () {
 }
 
 arch_mirror () {
+	echo "Updating mirrors"
+	pacman -S reflector -y
 	reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-cat << END
+cat << END > /etc/pacman.d/hooks/mirrorupgrade.hook
 [Trigger]
 Operation = Upgrade
 Type = Package
@@ -92,11 +121,11 @@ END
 gpu_driver () {
 	read -rp "What gpu are you using A-(AMD) B-(INTEL) C-(ATI) [A/B/C] : " gpu
 	if [ "$gpu" = "A" ]; then
-		pacman -S xf86-video-intel
+		pacman -S xf86-video-intel -y
 	elif [ "$gpu" = "B" ]; then
-		pacman -S xf86-video-amdgpu
+		pacman -S xf86-video-amdgpu -y
 	elif [ "$gpu" = "C" ]; then
-		pacman -S xf86-video-ati
+		pacman -S xf86-video-ati -y
 	else
 		echo "Invalid Answer"
 		gpu_driver
@@ -105,8 +134,13 @@ gpu_driver () {
 }
 
 setup_dotfiles () {
-	su $user_name
 	git clone https://github.com/tarwin1/.files.git
 	cd '.files'
-	stow *
+	stow -adopt *
 }
+
+if [ "$proceed" = "Y" ]; then
+	main
+else
+	exit 0
+fi
