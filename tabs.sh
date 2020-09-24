353e24ab888ef403c0cd82d71d2a2ff48ba84ff1
Murali <tarwin1@zohomail.com>
2020-09-24 15:36:36 +0800

    Lots of fixes

--- tabs.sh
+++ tabs.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-echo "WARNING: YOU ARE RUNNING TABS (Tarwin's Auto Bootstraping Script)"
-echo "Make sure you are running this as root"
-echo "Makes sure you have connected to internet"
-
-update () {
-	echo 'Updating...'
-	pacman --noconfirm --needed -Syyuu
-
-}
+echo 'WARNING: YOU ARE RUNNING TABS (Tarwin_s Auto Bootstraping Script)
+Make sure you are running this as root
+Makes sure you have connected to internet'
 
 create_user () {
-	echo "Creating User..."
 	echo "User name must be lower case and no space"
 	read -rp 'Enter you user name: ' user_name
 	user_name=$(echo "$user_name" | tr 'A-Z' 'a-z') # Changes user name to lowercase
@@ -19,14 +12,14 @@ create_user () {
 	}
 
 create_passwd () {
-	read -rp "Enter password for $user_name :" user_password
+	read -rp "Enter password for ${user_name}: " user_password
 	echo "${user_name}:${user_password}" | chpasswd
 	sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
 
 }
 
 pacman_install () {
-	sudo pacman --needed --noconfirm -S \
+	pacman --needed --noconfirm -S \
 		alsa-utils bspwm dash stow dmenu dunst git htop \
 		libnotify libva-utils linux-firmware man-db mlocate mpv neofetch \
 		neovim networkmanager newsboat noto-fonts noto-fonts-emoji picom \
@@ -39,28 +32,23 @@ pacman_install () {
 }
 
 systemctl_enable () {
-	sudo systemctl enable fstrim.timer
-	sudo systemctl enable NetworkManager
+	systemctl enable fstrim.timer
+	systemctl enable NetworkManager
 }
 
 microcode_install () {
-	echo "Installing microcode"
 	cpu_vendor=$(lscpu | grep Vendor | awk -F ': +' '{print $2}')
 	[ "$cpu_vendor" = "GenuineIntel" ] && pacman --needed --noconfirm -S intel-ucode && grub-mkconfig -o /boot/grub/grub.cfg
 	[ "$cpu_vendor" = "AuthenticAMD" ] && pacman --needed --noconfirm -S amd-ucode && grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 ssd_fstrim () {
-	read -rp "Are you using SSD [Y/n]" check_ssd
-	check_ssd=$( echo $check_ssd | tr A-Z a-z)
-	if [ "${check_ssd}" = "y" ] || [ -z "${check_ssd}" ]; then
-		systemctl enable fstrim.timer
-	fi
+	check_ssd=$(cat /sys/block/sd*/queue/rotational | paste -sd '+')
+	check_ssd=$((check_ssd))
+	[ $check_ssd -gt 0 ] && systemctl enable fstrim.timer
 }
 
 arch_mirror () {
-	echo "Updating mirrors"
-	mkdir /etc/pacman.d/hooks
 	pacman --needed -S --noconfirm  reflector
 	reflector --verbose --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 }
@@ -87,8 +75,10 @@ user_setup () {
 	makepkg -si
 	cd "$HOME"
 	rm -rf yay
+
 	# install aur pkg
 	yay  --noconfirm -Sy libxft-bgra polybar slock-gruvbox-lowcontrast st-luke-git
+
 	# setup dot files
 	cd "$HOME"
 	git clone https://github.com/tarwin1/.files.git
@@ -96,22 +86,41 @@ user_setup () {
 	stow --adopt *'
 }
 
+system_optimization () {
+	[ ! -d /etc/sysctl.d ] && mkdir /etc/sysctl.d
+	echo 'vm.swappiness = 10
+	vm.vfs_cache_pressure = 50
+	vm.watermark_scale_factor = 200
+	vm.dirty_ratio = 3' >/etc/sysctl.d/99-sysctl.conf
+	# Use all cores for compilation
+	sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+}
+
 main () {
 	read -rp "Proceed? [Y/n] " -n 1 continue
 	continue=$(echo "$continue" | tr A-Z a-z)
-	[ "$continue" = n ] && exit 0
+	[ "$continue" = n ] && exit
 	# Call all function
 	timedatectl set-ntp true # sets date and time correctly
-	update # full upgrade
+	echo 'Updating...'
+	pacman --noconfirm --needed -Syyuu > log
+	echo "Updating mirrors..."
 	arch_mirror # set mirror to the faster
+	echo "Creating User..."
 	create_user
 	create_passwd
-	microcode_install
-	ssd_fstrim
+	echo "Installing microcode"
+	microcode_install >> log
+	echo "Checkig for ssd..."
+	ssd_fstrim >> log
+	echo "Installing GPU driver"
 	gpu_driver
-	pacman_install
-	systemctl_enable
-	# Use all cores for compilation
-	sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
-	user_setup
+	echo "Installing packages..."
+	pacman_install >> log
+	echo "Enabling services..."
+	systemctl_enable >> log
+	echo "Setting up user..."
+	user_setup >> log
 }
+
+main
